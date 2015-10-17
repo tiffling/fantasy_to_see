8e7d0e96ce10bdf1ef8d042e87b6272aa12c7b1c
tiffling <tiffh@bu.edu>
2015-10-17 17:11:28 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YahooFantasyApp::Application.config.secret_key_base = '1f9648cd113a079fe71ab4afcfe3964329fb4008cc617d3575e46a193795ec107c99e194b3f82766011cbc127184656a4cf85cb7ad559b76c00e40bc59443e26'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YahooFantasyApp::Application.config.session_store :cookie_store, key: '_yahoo_fantasy_app_session'
