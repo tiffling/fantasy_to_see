35733b159f34a5fa810fc73fcc285654a8e97455
tiffling <tiffh@bu.edu>
2015-10-27 00:27:39 -0400

    make home page friendly for new users

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
   def index
-    @my_teams = Team.where(id: cookies[:my_team_ids].split(', '))
+    @my_teams = Team.where(id: cookies[:my_team_ids].to_s.split(', '))
   end
 end
